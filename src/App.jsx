7349ba7c8f9a0723ac4918517a5153f80b74e687
Matthew Freestone <matthewfreestone1593@gmail.com>
2022-10-04 19:47:24 -0500

    changed grand challenge

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
       <div className='my-5'>
         <h1 className='text-center font-bold text-3xl text-dark-blue text-primary'>Auburn ACM: GRAND</h1>
         {/* Link: http://www.engineeringchallenges.org/challenges/9109.aspx */}
-        <h3 className='text-center text-xl my-1'>Grand Challenge: <span className="font-bold">Reverse-Engineer the Brain </span></h3>
+        <h3 className='text-center text-xl my-1'>Grand Challenge: <span className="font-bold">Secure Cyberspace </span></h3>
         <p className='text-secondary text-md'>
           We want to make a computer that works like a real brain, but computers don't think like humans! 
-          Computers use <span class='font-bold'>binary</span> to store information, but the brain uses <span className='font-bold'>neurons</span> to store information.
+          Computers use <span class='font-bold'>binary</span> to store information.
           For this challenge, we want you to write your name in a way that a computer can understand. Try using an <span className='font-bold'>ASCII</span> table to help you. If you get stuck, try using the helper below.
         </p>
       </div>
